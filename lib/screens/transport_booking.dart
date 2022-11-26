36392d7a850ae5c00a904cd629e3f7ac930d5b6c
yoganlava <udeshya@hotmail.co.uk>
2022-11-26 19:36:49 +0000

    Just some stuff

---
+++ lib/screens/transport_booking.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TransportBookingPage extends StatefulWidget {
+  @override
+  State<TransportBookingPage> createState() => _TransportBookingPageState();
+}
+
+class _TransportBookingPageState extends State<TransportBookingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
